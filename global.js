cf2b20f712c8b6264eaea22d75346843ccd52b4a
Mark Bryce <markdbryce@gmail.com>
2023-11-20 18:26:52 +0000

    Created seperate pages and navbar in global.js

---
+++ global.js
@@ -0,0 +1,38 @@
+
+
+/* GLOBAL - Creating header title */
+const header = document.querySelector('#header');
+const headerTitle = document.createElement('h1');
+headerTitle.classList.add('pageTitle');
+headerTitle.textContent = "The Odin Project - Activities";
+header.appendChild(headerTitle);
+
+/* GLOBAL - Creating navbar and links */
+const nav = document.createElement('div');
+nav.classList.add("container", "main", "nav");
+header.appendChild(nav);
+
+let link1 = document.createElement('a');
+link1.setAttribute("href", "index.html");
+link1.textContent = "Home";
+nav.appendChild(link1);
+
+let link2 = document.createElement('a');
+link2.setAttribute("href", "shopping-list.html");
+link2.textContent = "Shopping List";
+nav.appendChild(link2);
+
+let link3 = document.createElement('a');
+link3.setAttribute("href", "rock-paper-scissors.html");
+link3.textContent = "Rock Paper Scissors";
+nav.appendChild(link3);
+
+let link4 = document.createElement('a');
+link4.setAttribute("href", "etch-a-sketch.html");
+link4.textContent = "Etch-a-Sketch";
+nav.appendChild(link4);
+
+let link5 = document.createElement('a');
+link5.setAttribute("href", "calculator.html");
+link5.textContent = "Calculator";
+nav.appendChild(link5);
